36f73291295fb6487548d52582fd7a0cb3057379
wibblybibblyfish <docherty.james.work@gmail.com>
2021-10-22 13:29:49 +0100

    manual ioc startup

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,10 @@
-import { Container, Scope } from "typescript-ioc";
-import { ICounterService, CounterService } from "./counterService";
+import { Container } from "typescript-ioc";
 import { CounterRunner } from "./counterRunner";
-import { NumberMatcherType } from "./numberMatcher";
-import { INumberMatcherFactory, NumberMatcherFactory } from "./numberMatcherFactory";
-import { INumberAnalyser, NumberAnalyser } from "./numberAnalyser";
-import { INumberFormatter, NumberFormatter } from "./numberFormatter";
+import { IoCInit } from "./ioc.config";
 
-Container.bindName('counterConfig').to({
-    startNum: 1,
-    endNum: 15,
-    increment: 1
-});
-
-Container.bindName('numberFormatConfig').to(
-   [{   
-        matcherType: NumberMatcherType.MutlipleOfThree,
-        replacementText: 'Fizz'
-    },
-    {
-        matcherType: NumberMatcherType.MultipleOfFive,
-        replacementText: 'Buzz'
-    }]
-);
-
-Container.bindName('matcherConfig').to(new Array(NumberMatcherType.MultipleOfFive, NumberMatcherType.MutlipleOfThree))
-
-Container.bind(ICounterService).to(CounterService).scope(Scope.Singleton);
-Container.bind(INumberMatcherFactory).to(NumberMatcherFactory).scope(Scope.Singleton);
-Container.bind(INumberAnalyser).to(NumberAnalyser).scope(Scope.Singleton);
-Container.bind(INumberFormatter).to(NumberFormatter).scope(Scope.Singleton);
+// TODO: can't get external binding file imports to work at present this is a workaround
+let initConfig = new IoCInit();
+initConfig.init();
 
 let runner = new CounterRunner();
 
---
+++ src/ioc.config.ts
@@ -0,0 +1,38 @@
+import { Container,  Scope } from 'typescript-ioc';
+import { CounterService, ICounterService } from './counterService';
+import { INumberAnalyser, NumberAnalyser } from './numberAnalyser';
+import { INumberFormatter, NumberFormatter } from './numberFormatter';
+import { NumberMatcherType } from './numberMatcher';
+import { INumberMatcherFactory, NumberMatcherFactory } from './numberMatcherFactory';
+
+export class IoCInit {
+
+    public init(): void {
+Container.bindName('counterConfig').to({
+    startNum: 1,
+    endNum: 15,
+    increment: 1
+});
+
+Container.bindName('numberFormatConfig').to(
+   [{   
+        matcherType: NumberMatcherType.MutlipleOfThree,
+        replacementText: 'Fizz'
+    },
+    {
+        matcherType: NumberMatcherType.MultipleOfFive,
+        replacementText: 'Buzz'
+    }]
+);
+
+Container.bindName('matcherConfig').to(new Array(NumberMatcherType.MultipleOfFive, NumberMatcherType.MutlipleOfThree))
+
+Container.bind(ICounterService).to(CounterService).scope(Scope.Singleton);
+Container.bind(INumberMatcherFactory).to(NumberMatcherFactory).scope(Scope.Singleton);
+Container.bind(INumberAnalyser).to(NumberAnalyser).scope(Scope.Singleton);
+Container.bind(INumberFormatter).to(NumberFormatter).scope(Scope.Singleton);
+    }
+
+}
+
+
